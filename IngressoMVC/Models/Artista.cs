a7952a8033ee82b6041a8eaac60e221b8299ada5
KaueSenac <kaue.gfenerick@senacsp.edu.br>
2022-05-19 19:35:14 -0300

    Mudanca estrutural do projeto

--- IngressoMVC/Models/Artista.cs
+++ IngressoMVC/Models/Artista.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using IngressoMVC.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IngressoMVC.Models
 {
-    public class Artista : IEntidade
+    abstract public class Artista : IEntidade
     {
         public int Id { get; set; }
         public DateTime DataCadastro { get; set; }
