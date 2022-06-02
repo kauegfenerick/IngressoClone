9daf5f7b4101b6fe48db5ff773049c59e7c8b428
KaueSenac <kauegomesfenerick@gmail.com>
2022-06-01 21:52:49 -0300

    Metódo Criar e Views de: Produtor, categoria e cinema

---
+++ IngressoMVC/Models/ViewModels/Request/PostCategoriaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IngressoMVC.Models.ViewModels.Request
+{
+    public class PostCategoriaDTO
+    {
+        public string Categoria { get; set; }
+    }
+}
---
+++ IngressoMVC/Models/ViewModels/Request/PostCinemaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IngressoMVC.Models.ViewModels.Request
+{
+    public class PostCinemaDTO
+    {
+        public string Nome { get; set; }
+        public string Descricacao { get; set; }
+        public string LogoURL { get; set; }
+    }
+}
